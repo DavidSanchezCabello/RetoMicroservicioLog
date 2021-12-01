d48c7c43f5f42e74860eb5338f6ee82470457956
David <david.cabello.info@gmail.com>
2021-12-01 09:47:57 +0100

    first commit

--- src/main/java/com/dscode/aplication/controller/TestController.java
+++ src/main/java/com/dscode/aplication/controller/TestController.java
@@ -2,31 +2,48 @@ package com.dscode.aplication.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @RestController
 public class TestController {
 
-	@Value("${some.value}")
-	private String myValue;
+	private Counter counterCel;
+	private Counter counterFar;
 	
-	@GetMapping(path="/myValue")
-	public String myValue() {
-		return this.myValue;
-	}
 	
-
 	private final static Logger logger= LoggerFactory.getLogger(TestController.class);
 	
-	
-	@GetMapping("/")
-	public ResponseEntity<String> index() {
-		logger.info("Llamada al endpoint inicial." + this.myValue);
-		return new ResponseEntity<String>(HttpStatus.OK).ok("Hola Reto" + this.myValue);
+	public TestController(MeterRegistry registro) {
+		this.counterCel = Counter.builder("invocaciones.Celsius").description("Invocaciones totales").register(registro);
+		this.counterFar = Counter.builder("invocaciones.Fahrenheit").description("Invocaciones totales").register(registro);
 	}
 	
+	@GetMapping(path="/testCelsius")
+	public String testCelsius() {
+		counterCel.increment();
+		return "Celsius";
+	}
+	
+	@GetMapping(path="/testFarhen")
+	public String holaMundo() {
+		counterFar.increment();
+		return "Fahrenheit";
+	}
+	
+	@GetMapping(path="/celAFar/{celsius}")
+	public static float celsiusAFahrenheit(@PathVariable float celsius) {
+		logger.info("Llamada al endpoint celsius.");
+		return (celsius * 1.8f) + 32;
+	}
+	
+	@GetMapping(path="/farACel/{fahrenheit}")
+	public static float fahrenheitACelsius(@PathVariable float fahrenheit) {
+		logger.info("Llamada al endpoint fahrenheit.");
+		return (fahrenheit - 32) / 1.8f;
+	}
 }
